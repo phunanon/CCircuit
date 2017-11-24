9b007bbb407e386590ade579534f2df7d4bc115d
phunanon <phunanon@gmail.com>
2017-11-24 15:50:08 +0000

    Cleaned the file

--- keypresses.c
+++ keypresses.c
@@ -1,7 +1,7 @@
-#include <sys/time.h> //For time
-#include <termios.h> //For key presses
-#include <stdlib.h> //For key presses
-#include <string.h> //For key presses
+#include <sys/time.h> // For time
+#include <termios.h>  //
+#include <stdlib.h>   //
+#include <string.h>   // For key presses
 
 
 //===================================
@@ -11,27 +11,31 @@
 static struct termios g_old_kbd_mode;
 static char pressedCh;
 
-// did somebody press somthing???
-static int kbhit(void){
+//Check if a key has been pressed
+static int kbhit (void)
+{
     struct timeval timeout;
     fd_set read_handles;
 
-    // check stdin (fd 0) for activity
+    //Check stdin (fd 0) for activity
     FD_ZERO(&read_handles);
     FD_SET(0, &read_handles);
     timeout.tv_sec = timeout.tv_usec = 0;
-    return select(1, &read_handles, NULL, NULL, &timeout); //Return status
+  //Return status
+    return select(1, &read_handles, NULL, NULL, &timeout);
 }
 
-// put the things as they were befor leave..!!!
-static void old_attr(void){
+//Restore terminal state
+static void old_attr (void)
+{
     tcsetattr(0, TCSANOW, &g_old_kbd_mode);
 }
 
-static void loadKeyListen(void) //For listening to keys pressed
+//Prepare to listen to key presses
+static void loadKeyListen (void)
 {
     struct termios new_kbd_mode;
-    // put keyboard (stdin, actually) in raw, unbuffered mode
+  //Set stdin to raw, unbuffered mode
     tcgetattr(0, &g_old_kbd_mode);
     memcpy(&new_kbd_mode, &g_old_kbd_mode, sizeof(struct termios));
     
