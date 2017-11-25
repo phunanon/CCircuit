261190160898d2fc59b15fd7446d554d8489f9bc
phunanon <phunanon@gmail.com>
2017-11-25 22:28:28 +0000

    Change output extension

--- comp.sh
+++ comp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-[ -z $1 ] && g++ CCircuit.cpp -o CCircuit.elf --std=c++11 -g && exit 1
-g++ CCircuit.cpp -o CCircuit.elf --std=c++11 -static-libstdc++ -O3
+[ -z $1 ] && g++ CCircuit.cpp -o CCircuit.out --std=c++11 -g && exit 1
+g++ CCircuit.cpp -o CCircuit.out --std=c++11 -static-libstdc++ -O3
