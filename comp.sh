eab2b784b2c0cecb2fea01019508411939250c08
phunanon <phunanon@gmail.com>
2017-11-25 22:24:14 +0000

    Added crude conditional release & debug

--- comp.sh
+++ comp.sh
@@ -1,2 +1,4 @@
-g++ CCircuit.cpp -o CCircuit.elf --std=c++11 -g
-#g++ CCircuit.cpp -o CCircuit.elf --std=c++11 -static-libstdc++ -O3 -g
+#!/bin/bash
+
+[ -z $1 ] && g++ CCircuit.cpp -o CCircuit.elf --std=c++11 -g && exit 1
+g++ CCircuit.cpp -o CCircuit.elf --std=c++11 -static-libstdc++ -O3
