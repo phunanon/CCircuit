e494cd173ec29f26bc51832e7b6a839913a11290
phunanon <phunanon@gmail.com>
2017-12-25 14:00:28 +0000

    Update

--- dirty_compile.sh
+++ dirty_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Compile as either debug or supply any argument to release
-[ -z $1 ] && g++ src/*.hpp src/*.cpp -o CCircuit.out --std=c++11 -g && exit 1
-g++ src/*.hpp src/*.cpp -o dir/CCircuit.out --std=c++11 -static-libstdc++ -O3
+[ -z $1 ] && g++ src/*.hpp src/*.cpp -o CCircuit --std=c++17 -g && exit 1
+g++ src/*.hpp src/*.cpp -o dir/CCircuit --std=c++17 -static-libstdc++ -O3
